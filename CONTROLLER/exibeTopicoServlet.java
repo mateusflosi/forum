d077c84d3fb3f16a6d83a4f3431018243ecfc8b8
Mateus Flosi Molero <mateusflosimolero@hotmail.com>
2020-08-28 00:50:27 -0300

    Update exibeTopicoServlet.java

--- CONTROLLER/exibeTopicoServlet.java
+++ CONTROLLER/exibeTopicoServlet.java
@@ -10,38 +10,39 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "exibeTopicoServlet", urlPatterns = {"/exibeTopico"})
 public class exibeTopicoServlet extends HttpServlet {
+    
+    protected void processamento(HttpServletRequest request, HttpServletResponse response,
+            String titulo, String login, String conteudo, int id) throws ServletException, IOException {
+        TopicosDAO t = new TopicosDAO();
+        UsuarioDAO u = new UsuarioDAO();
+        ComentarioDAO c = new ComentarioDAO();
+        request.setAttribute("titulo", titulo);
+        request.setAttribute("login", login);
+        request.setAttribute("conteudo", conteudo);
+        request.setAttribute("id", id);
+        request.setAttribute("autor", u.getNome(t.getLogin(id)));
+        request.setAttribute("comentarios", c.comentariosDoTopico(id));
+        request.getRequestDispatcher("exibeTopico.jsp").forward(request,response);
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        TopicosDAO t = new TopicosDAO();
-        UsuarioDAO u = new UsuarioDAO();
-        ComentarioDAO c = new ComentarioDAO();
         String titulo = (String) request.getParameter("titulo");
         String login = (String) request.getParameter("login");
-        request.setAttribute("titulo", titulo);
-        request.setAttribute("login", login);
-        request.setAttribute("conteudo", t.getConteudo(titulo));
-        request.setAttribute("autor", u.getNome(t.getLogin(titulo)));
-        request.setAttribute("comentarios", c.comentariosDoTopico(t.getId(titulo)));
-        request.getRequestDispatcher("exibeTopico.jsp").forward(request,response);
+        String conteudo = (String) request.getParameter("conteudo");
+        int id = Integer.parseInt((String) request.getParameter("id"));
+        processamento(request, response, titulo, login, conteudo, id);
     }
     
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        TopicosDAO t = new TopicosDAO();
-        UsuarioDAO u = new UsuarioDAO();
-        ComentarioDAO c = new ComentarioDAO();
         String erro = (String) request.getAttribute("erro");
         String titulo = (String) request.getAttribute("titulo");
         String login = (String) request.getAttribute("login");
-        request.setAttribute("titulo", titulo);
-        request.setAttribute("login", login);
-        request.setAttribute("erro", erro);
-        request.setAttribute("conteudo", t.getConteudo(titulo));
-        request.setAttribute("autor", u.getNome(t.getLogin(titulo)));
-        request.setAttribute("comentarios", c.comentariosDoTopico(t.getId(titulo)));
-        request.getRequestDispatcher("exibeTopico.jsp").forward(request,response);
+        String conteudo = (String) request.getAttribute("conteudo");
+        int id = Integer.parseInt((String) request.getAttribute("id"));
+        processamento(request, response, titulo, login, conteudo, id);
     }
 }
